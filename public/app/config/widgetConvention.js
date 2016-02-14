7513a45eb3f556ec5825c2a94835f939d04f65dd
danyg <dalgo86@gmail.com>
2016-02-14 05:45:08 -0800

    DG [] ADD: WidgetConvention, easier to use with Ctrl+P/Ctrl+Shift+R not everything is viewmodel.js (god sake...) also allow to do "private" widgets in a module RMV: some log traces in client

---
+++ public/app/config/widgetConvention.js
@@ -0,0 +1,39 @@
+define([
+	'plugins/widget'
+],
+function(
+	widget
+){
+	'use strict';
+
+	function getParts(kind, type) {
+		var parts = {
+			container: '',
+			subpath: ''
+		};
+		var tmp = kind.split('/');
+		parts.container = tmp.splice(0,1);
+		parts.subpath = tmp.join('/') + type;
+		return parts;
+	}
+
+	function getPath(kind, type) {
+		if(kind.indexOf('/') === -1) {
+			return 'widgets/' + kind + '/' + kind + type;
+		} else {
+			var parts = getParts(kind, type);
+			return parts.container + '/widgets/' + parts.subpath;
+		}
+	}
+
+	function useConvention(){
+		widget.convertKindToModulePath = function(kind){
+			return getPath(kind, 'Widget');
+		};
+		widget.convertKindToViewPath = function(kind){
+			return getPath(kind, 'View');
+		};
+	}
+
+	return useConvention;
+});
